2034421c25048322e5e3e957016ac50fb4b5e91d
Robert <krstb-90@yahoo.com>
2026-01-05 15:59:01 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'notizen_online-v2'; // Ändere v2 zu v3, v4 etc., um Updates zu erzwingen
+const CACHE_NAME = 'notizen_online-v3'; // Ändere v2 zu v3, v4 etc., um Updates zu erzwingen
 const ASSETS = [
   'index.html',
   'manifest.json'
